ea2a75ae5eef58945ab0187e9d8b4178716bc460
Stefanie Wenger <WENGER STEFANIE@paed.gympu.de>
2017-05-11 09:18:52 +0200

    keine enums piece

--- src/KNIGHT.java
+++ src/KNIGHT.java
@@ -3,7 +3,7 @@ public class KNIGHT extends PIECE
 	public KNIGHT(KIND kindNeu, COLOR colorNeu,int xNeu, int yNeu)
 	{
 		super(kindNeu, colorNeu, xNeu, yNeu);
-		kind = KIND.kn;
+		kind = "N";
 	}
 	
 
