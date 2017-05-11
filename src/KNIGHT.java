49d46111b92ebf2028f659e55bd6715b40b5d4d0
Stefanie Wenger <WENGER STEFANIE@paed.gympu.de>
2017-05-11 08:15:06 +0200

    init commit

---
+++ src/KNIGHT.java
@@ -0,0 +1,9 @@
+public class KNIGHT extends PIECE
+{
+	public KNIGHT(KIND kindNeu, COLOR colorNeu,int xNeu, int yNeu)
+	{
+		super(kindNeu, colorNeu, xNeu, yNeu);
+	}
+	
+
+}
---
+++ src/PAWN.java
@@ -0,0 +1,10 @@
+public class PAWN extends PIECE
+{
+	
+public PAWN(KIND kindNeu, COLOR colorNeu,int xNeu, int yNeu)
+{
+	super(kindNeu, colorNeu, xNeu, yNeu);
+}
+
+
+}
